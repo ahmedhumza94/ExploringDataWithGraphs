e27a88d4c78ee6d7a0d124f12e54cff6ba583036
ahmedhumza94 <ahmedhumza94@gmail.com>
2018-07-15 13:05:06 -0500

    feat: Day 1 Hygeine Score Boxplots

--- DownloadFestivalVisualizations.R
+++ DownloadFestivalVisualizations.R
@@ -13,3 +13,18 @@ festivalHistogram <- ggplot(festData, aes(day1)) + theme(legend.position = "none
 festivalHistogram + 
   labs(x = "Day 1 Rating",y="Frequency", title = "Day 1 Feature Histogram") +
   geom_histogram(binwidth = 0.4)
+
+#Boxplot of day 1 scores by gender
+genderBoxplot <- ggplot(festData,aes(gender,day1))
+genderBoxplot + geom_boxplot() + labs(x="Gender",y="Day 1 Scores")
+
+#Order the data by day 1 scores
+festData <- festData[order(festData$day1),]
+#View Last element
+tail(festData,n=1)
+#Correct outlier to be 2.02 instead of 20.02
+festData$day1[nrow(festData)] <- 2.02
+
+#Create new boxplot with corrected outlier
+genBoxplotCor <- ggplot(festData, aes(gender,day1))
+genBoxplotCor + geom_boxplot() + labs(x="Gender",y="Day 1 Scores")
